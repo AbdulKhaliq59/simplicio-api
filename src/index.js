dc6caef6f847324d6bdbef34011ab7d33a8d6847
abdulkhaliq <kananuraabdulkhaliq59@gmail.com>
2024-03-04 11:26:35 +0200

    Fixing mongo error

--- src/index.js
+++ src/index.js
@@ -6,10 +6,7 @@ dotenv.config();
 
 const port = process.env.PORT || 3000;
 
-mongoose.connect(process.env.MONGO_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(process.env.MONGO_URL);
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.once('open', () => {
